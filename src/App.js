f2fd0fdf8cb6cec5a899886b0d345fae99c8e687
zask <uthetnaingtun223@gmail.com>
2023-12-28 21:03:13 +0630

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function Item(props) {
+//   return <li>{props.name}, ${props.price}</li>;
+// }
 
-export default App;
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Hello React</h1>
+//         <ul>
+//           <Item name="🍎" price="0.99" />
+//           <Item name="🍊"  price="1.99"/>
+//         </ul>
+//     </div>
+//   );
+// }
+
+
+// export default App;
+
+
+// use State
+
+
+import React from 'react';
+
+import { useState } from 'react';
+
+function Item(props) {
+  return <li>{props.name}, {props.price}</li>
+
+  }
+
+  export default function App() {
+    const [data, setData] = useState([
+      { id: 1, name:"🍎" , price: 0.99},
+      { id: 1, name:"🍊" , price: 0.98},
+    ]);
+
+    const add = () => {
+      const id = data.length + 1;
+
+      setData([
+        ...data,
+        {id, name: `Item${id}`, price: 0.01 *id }
+      ])
+    }
+
+    return (
+      <div>
+        <h1> Hello useState 🚀</h1>
+          <ul>
+            {data.map(i => (
+              <Item name={i.name} price={i.price} />
+            ))}
+          </ul>
+          <button onClick={add}>Add</button>
+      </div>
+    )
+  }
+
